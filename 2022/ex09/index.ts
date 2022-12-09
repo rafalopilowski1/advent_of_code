4a6eefbc7001f0227ac1621d42cce26950d2d67f
Rafał Opiłowski <rafalopilowski1@outlook.com>
2022-12-09 23:19:18 +0100

    Day 9 - only 1 star :(

--- 2022/ex09/index.ts
+++ 2022/ex09/index.ts
@@ -1,7 +1,7 @@
 import { readFile } from "fs/promises";
 
 enum Direction {
-    Up, Down, Left, Right
+    Up = "U", Down = "D", Left = "L", Right = "R",
 }
 
 class Move {
@@ -14,22 +14,65 @@ class Move {
     }
 }
 
-class Head implements Moveable {
-    "move": (moveToDo: Move) => void;
-}
-
-class Tail implements Moveable {
-    "move": (moveToDo: Move) => void;
+class Node {
+    currentPosition: Position;
+    previousPosition: Position | null;
+    next: Node | null;
     visited: Position[];
+    static move(moveToDo: Move, current: Node) {
+        for (let i = 0; i < moveToDo.amount; i++) {
+            current.previousPosition = { ...current.currentPosition };
+            switch (moveToDo.direction) {
+                case Direction.Up: {
+                    current.currentPosition.y += 1;
+                    break;
+                }
+                case Direction.Down: {
+                    current.currentPosition.y -= 1;
+                    break;
+                }
+                case Direction.Left: {
+                    current.currentPosition.x -= 1;
+                    break;
+                }
+                case Direction.Right: {
+                    current.currentPosition.x += 1;
+                    break;
+                }
+            }
+            if (current.visited.filter(visit => visit.x == current.currentPosition.x && visit.y == current.currentPosition.y).length == 0) current.visited.push({ ...current.currentPosition });
+            if (current.next !== null) {
+                let currentTemp = current;
+                while (currentTemp.next !== null) {
+                    Node.moveNextNodesClose(currentTemp);
+                    currentTemp = currentTemp.next;
+                }
+            }
+        }
+    }
+    constructor() {
+        this.next = null;
+        this.currentPosition = { x: 0, y: 0 };
+        this.previousPosition = null;
+        this.visited = [];
+        this.visited.push({ ...this.currentPosition });
+    }
+
+    private static moveNextNodesClose(current: Node) {
+        const xDifference = Math.abs(current.currentPosition.x - current.next.currentPosition.x);
+        const yDifference = Math.abs(current.currentPosition.y - current.next.currentPosition.y);
+        if (xDifference > 1 || yDifference > 1) {
+            current.next.previousPosition = { ...current.next.currentPosition };
+            current.next.currentPosition = { ...current.previousPosition };
+            if (current.next.visited.filter(visit => visit.x == current.next.currentPosition.x && visit.y == current.next.currentPosition.y).length == 0)
+                current.next.visited.push({ ...current.next.currentPosition });
+        }
+    }
 }
 
 interface Position {
-    "x":number,
-    "y":number,
-}
-
-interface Moveable {
-    "move": (moveToDo: Move) => void;
+    x: number,
+    y: number,
 }
 
 const data = await readFile("./puzzle_input.txt");
@@ -48,12 +91,25 @@ const moves = data.toString("utf-8").split("\n").map(move => {
             throw new Error("Invalid input (no direction)");
     }
 });
-console.log(moves);
-const ropeBridge: Moveable[][] = [];
+let head = new Node();
+const tail = new Node();
+head.next = tail;
+// Part 1
+moves.forEach(move => Node.move(move, head));
+console.log(head);
+console.log("Part 1: " + tail.visited.length);
 
-moves.forEach(move => {
+// Part 2 - faulty :( 
 
-});
-
-
-
+head = new Node();
+let temp: Node = head;
+for (let i = 1; i <= 10; i++) {
+    temp.next = new Node();
+    temp = temp.next;
+}
+moves.forEach(move => Node.move(move, head));
+temp = head;
+while (temp.next != null) {
+    console.log(temp.visited.length);
+    temp = temp.next;
+}
