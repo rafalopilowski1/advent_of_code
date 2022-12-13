785426dda79c6554d0e1efcfa81815a68118ad6a
Rafał Opiłowski <rafalopilowski1@outlook.com>
2022-12-13 01:09:03 +0100

    Simplify Day 12

--- 2022/ex12/Graph.ts
+++ 2022/ex12/Graph.ts
@@ -11,31 +11,20 @@ export class Graph {
         }
         return foundNodes[0];
     };
-    minDistance = (dist: Map<Node, number>, sptSet: Map<Node, boolean>): Node => {
-        let min = Number.MAX_VALUE;
-        let minNode: Node;
-        this.nodes.filter(node => !sptSet.get(node)).forEach(node => {
-            if (dist.get(node) <= min) {
-                min = dist.get(node);
-                minNode = node;
-            }
-        });
-        return minNode;
-    };
-
+    minDistance = (dist: Map<Node, number>, sptSet: Map<Node, boolean>): Node => this.nodes.filter(node => !sptSet.get(node)).sort((nodeA, nodeB) => dist.get(nodeA) - dist.get(nodeB))[0];
     dijkstra = (src: Node): Map<Node, number> => {
         const distance: Map<Node, number> = new Map();
         const visitedNodes: Map<Node, boolean> = new Map();
         this.nodes.forEach(node => {
-            distance.set(node, Number.MAX_VALUE);
+            distance.set(node, Number.POSITIVE_INFINITY);
             visitedNodes.set(node, false);
         });
         distance.set(src, 0);
         this.nodes.forEach(() => {
             const nearestNode = this.minDistance(distance, visitedNodes);
             visitedNodes.set(nearestNode, true);
-            nearestNode.connections.forEach((node) => {
-                if (!visitedNodes.get(node.destNode) && distance.get(nearestNode) + node.cost <= distance.get(node.destNode))
+            nearestNode.connections.filter(node => !visitedNodes.get(node.destNode)).forEach(node => {
+                if (distance.get(nearestNode) + node.cost <= distance.get(node.destNode))
                     distance.set(node.destNode, distance.get(nearestNode) + node.cost);
             });
         });
